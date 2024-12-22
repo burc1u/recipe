a29e56e35b85cd82221c13f84b68ea0986fda17e
burc1u <133819413+burc1u@users.noreply.github.com>
2024-12-22 22:26:38 +0200

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/"
 export const TIMEOUT_SEC = 10
 export const RES_PER_PAGE = 10
-export const API_KEY = "f7da5084-0553-487b-a43c-416de427e3c3"
+export const API_KEY = "876dda76-4a34-4c9a-ab2a-73fc5575dc8b"
 export const MODAL_CLOSE_SEC = 2.5
